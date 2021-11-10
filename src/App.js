695bf94145bb2a3866d7e616ce78eb708bc3573b
Mohammad Mostafa <mohammad-mostafa94>
2021-11-10 21:31:21 +0600

    v5 react dom added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Route, Switch
+} from "react-router-dom";
 import './App.css';
+import HomePage from './Pages/HomePage/HomePage';
+import Footer from "./Pages/Shared/Footer/Footer";
+import Navigation from "./Pages/Shared/Nevigatioin/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bismillah </h1>
+    <div>
+      {/* <h1>Bismillah </h1>*/}
+      <HomePage></HomePage> 
+      <Router>
+      <Navigation/>
+        <Switch>
+          <Route path="/" component={HomePage}/>
+          <Route path="/home" component={HomePage}/>
+        </Switch>
+        <Footer/>
+    </Router>
     </div>
   );
 }
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './Banner/Banner';
+import Products from './Products/Products';
+import Reviews from './Reviews/Reviews';
+
+const HomePage = () => {
+    return (
+        <div>
+            
+            <Banner></Banner>
+            <Products></Products>
+            <Reviews></Reviews>
+            
+        </div>
+    );
+};
+
+export default HomePage;
